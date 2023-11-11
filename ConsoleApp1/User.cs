51f93065324da8d4824923ca8a2661707326e9fa
Mohammad Alsuleibi <201106@ppu.edu.ps>
2023-11-11 04:37:54 +0200

    second commit

--- ConsoleApp1/User.cs
+++ ConsoleApp1/User.cs
@@ -1,6 +1,30 @@
 
-namespace MyApp
+namespace CSharp_Project
 {
+    public class User
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        public bool Verify()
+        {
+            // TODO: Implement verification logic
+            return true;
+        }
+
+        public bool CheckAccount()
+        {
+            // TODO: Implement account checking logic
+            return true;
+        }
+
+        public string Get_book_info(string bookName)
+        {
+            // TODO: Implement book info retrieval logic
+            return $"Information about {bookName}";
+        }
+    }
+}
 {
     public class User
     {
